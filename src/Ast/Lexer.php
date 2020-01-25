9254a2815beb799453e31aeb6d25284b733d0931
Ksaveras Šakys <xawiers@gmail.com>
2020-01-25 14:09:29 +0000

    Lexer improvements

--- src/Ast/Lexer.php
+++ src/Ast/Lexer.php
@@ -2,11 +2,21 @@
 
 namespace Ksaveras\MathCalculator\Ast;
 
+use Ksaveras\MathCalculator\Ast\Node\MinusOperator;
+use Ksaveras\MathCalculator\Ast\Node\NodeInterface;
+use Ksaveras\MathCalculator\Ast\Node\Number;
+use Ksaveras\MathCalculator\Ast\Node\OperatorInterface;
+
 /**
  * Class Lexer.
  */
 class Lexer
 {
+    /**
+     * @param string $input
+     *
+     * @return NodeInterface[]|array
+     */
     public function parse(string $input): array
     {
         $tokens = token_get_all('<?php '.$input);
@@ -15,10 +25,10 @@ class Lexer
         $nodeFactory = new NodeFactory();
 
         $nodeStream = [];
-        foreach($tokens as $token) {
+        foreach ($tokens as $token) {
             if (is_string($token)) {
                 $value = $token;
-            } elseif(isset($token[0], $token[1]) && T_WHITESPACE !== $token[0]) {
+            } elseif (isset($token[0], $token[1]) && T_WHITESPACE !== $token[0]) {
                 $value = $token[1];
             } else {
                 $value = null;
@@ -30,5 +40,51 @@ class Lexer
         }
 
         return $nodeStream;
+    }
+
+    public function buildReversePolishNotation(array $nodeStream): array
+    {
+        $output = [];
+        $stack = [];
+        $lastNode = null;
+
+        foreach ($nodeStream as $node) {
+            switch (true) {
+                case $node instanceof Number:
+                    $output[] = $node;
+                    break;
+                case $node instanceof OperatorInterface:
+                    if ($lastNode instanceof OperatorInterface && $node instanceof MinusOperator) {
+                        $output[] = new Number(0);
+                        $node->setAssociation(OperatorInterface::RIGHT_ASSOC);
+                        $node->setPriority(100);
+                    }
+
+                    while (
+                        ($count = count($stack)) > 0
+                        && ($stack[$count - 1] instanceof OperatorInterface
+                            && (
+                                ($stack[$count - 1]->getAssociation() === OperatorInterface::LEFT_ASSOC
+                                    && $stack[$count - 1]->getPriority() === $node->getPriority())
+                                || ($stack[$count - 1]->getPriority() > $node->getPriority())
+                            )
+                        )
+                    ) {
+                        $output[] = array_pop($stack);
+                    }
+
+                    $stack[] = $node;
+                    break;
+            }
+
+            $lastNode = $node;
+        }
+
+        while (!empty($stack)) {
+            $node = array_pop($stack);
+            $output[] = $node;
+        }
+
+        return $output;
     }
 }
