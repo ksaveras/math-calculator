887bb7cee949a8119b9172813f479ba4eb33befa
Ksaveras Sakys <xawiers@gmail.com>
2020-09-15 08:18:03 +0100

    Refactor node factory

--- src/Ast/NodeFactory.php
+++ src/Ast/NodeFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ksaveras\MathCalculator\Ast;
 
+use Ksaveras\MathCalculator\Ast\Node\AbstractOperator;
 use Ksaveras\MathCalculator\Ast\Node\BitwiseAndOperator;
 use Ksaveras\MathCalculator\Ast\Node\BitwiseOrOperator;
 use Ksaveras\MathCalculator\Ast\Node\DivisionOperator;
@@ -18,6 +19,23 @@ use Ksaveras\MathCalculator\Ast\Node\PlusOperator;
  */
 class NodeFactory
 {
+    /**
+     * @var array<int, AbstractOperator>|AbstractOperator[]
+     */
+    private static $operatorObjects = [];
+
+    /**
+     * @var string[]|array<string, string>
+     */
+    private static $operatorMap = [
+        '+' => PlusOperator::class,
+        '-' => MinusOperator::class,
+        '*' => MultiplyOperator::class,
+        '/' => DivisionOperator::class,
+        '|' => BitwiseOrOperator::class,
+        '&' => BitwiseAndOperator::class,
+    ];
+
     public function createNode(string $token): NodeInterface
     {
         if (is_numeric($token)) {
@@ -26,25 +44,13 @@ class NodeFactory
             return new Number($token);
         }
 
-        if ('+' === $token) {
-            return new PlusOperator();
-        }
-        if ('-' === $token) {
-            return new MinusOperator();
-        }
-        if ('*' === $token) {
-            return new MultiplyOperator();
-        }
-        if ('/' === $token) {
-            return new DivisionOperator();
-        }
-        if ('|' === $token) {
-            return new BitwiseOrOperator();
-        }
-        if ('&' === $token) {
-            return new BitwiseAndOperator();
+        if (!isset(self::$operatorObjects[$token])) {
+            if (!isset(static::$operatorMap[$token])) {
+                throw new \LogicException(sprintf('Unsupported token "%s"', $token));
+            }
+            self::$operatorObjects[$token] = new self::$operatorMap[$token]();
         }
 
-        throw new \LogicException(sprintf('Unsupported token "%s"', $token));
+        return self::$operatorObjects[$token];
     }
 }
