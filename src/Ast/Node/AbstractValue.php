602a67658e179c574c2917ae3d9876a51f7e1627
Ksaveras Sakys <xawiers@gmail.com>
2020-09-16 07:33:07 +0100

    Add phpdoc annotations

--- src/Ast/Node/AbstractValue.php
+++ src/Ast/Node/AbstractValue.php
@@ -16,16 +16,25 @@ abstract class AbstractValue implements NodeInterface
      */
     private $value;
 
+    /**
+     * @param mixed $value
+     */
     public function __construct($value)
     {
         $this->value = $value;
     }
 
+    /**
+     * @return mixed
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * @param mixed $value
+     */
     public function setValue($value): void
     {
         $this->value = $value;
